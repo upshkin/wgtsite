1803780cb9ddde28b11a0081417136e5500dab32
upshkin <upshkin@gmail.com>
2018-05-14 01:13:23 +0300

    Добавил форму и страницу контактов

--- blog/models.py
+++ blog/models.py
@@ -8,11 +8,16 @@ from taggit.models import TaggedItemBase
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel, FieldRowPanel,
+    InlinePanel, MultiFieldPanel
+)
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
 from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.contrib.forms.edit_handlers import FormSubmissionsPanel
 
 
 class BlogPage(Page):
@@ -42,6 +47,7 @@ class BlogPostTag(TaggedItemBase):
 
 
 class BlogTagPage(Page):
+    subpage_types = []
 
     def get_context(self, request):
         # Filter by tag
@@ -134,3 +140,32 @@ class SocialMediaSettings(BaseSetting):
 
     class Meta:
         verbose_name = 'Social settings'
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'FormPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields'
+    )
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro', classname='full'),
+        InlinePanel('form_fields', label='Form Fields'),
+        FieldPanel('thank_you_text', classname='full'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname='col6'),
+                FieldPanel('to_address', classname='col6'),
+            ]),
+            FieldPanel('subject'),
+        ], 'Email'),
+    ]
+
+    subpage_types = []
